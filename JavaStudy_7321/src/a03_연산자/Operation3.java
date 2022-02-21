97dc10d784f32bee3dd1b1204f8a307dcb528c7c
DESKTOP-GU4PRF4\ehgns <rlaehgns69@naver.com>
2022-02-21 12:49:48 +0900

    연산자 조건 삼항

---
+++ JavaStudy_7321/src/a03_연산자/Operation3.java
@@ -0,0 +1,16 @@
+package a03_연산자;
+
+public class Operation3 {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		
+		int result=(num1 > num2) ? 1:2;
+		System.out.println(result);
+		
+		boolean result2=num1>num2 ? true:false;
+		System.out.println(result2);
+	}
+
+}
